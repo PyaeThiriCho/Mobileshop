e6907298b8c27a04264db9727197c2ffdc968220
PyaeThiriCho <pyaethiricho4@gmail.com>
2025-10-28 23:43:54 +0630

    first

---
+++ routes/web.php
@@ -0,0 +1,23 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\BrandController;
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider and all of them will
+| be assigned to the "web" middleware group. Make something great!
+|
+*/
+
+// Route::get('/', function () {
+//     return view('welcome');
+// });
+
+Route::get('/table',[App\Http\Controllers\BackendTemplateController::class, 'tablefun'])->name('table');
+
+Route::resource('brands',BrandController::class);
